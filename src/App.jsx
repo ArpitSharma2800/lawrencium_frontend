6db32890f598027e65c0568a612d2c806df2ad7f
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-03-28 09:42:12 +0530

    added material ui and homepage boiler plate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Test from "./test/test.jsx";
 import "./App.css";
+import Home from "./Components/homePage/home.jsx";
 export default function App() {
   return (
     <div className="App">
-      <Test />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/homePage/home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import AppBarHome from './src/appbar'
+import OutlinedCard from './src/fileList'
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from '@material-ui/core';
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1
+    },
+    title: {
+        marginLeft: 10
+    }
+}));
+export default function Home() {
+    const classes = useStyles();
+    return (
+        <div>
+            <AppBarHome />
+            <Typography className={classes.title} variant="h6" component="h2">
+                Previously Uploaded Files:
+                    </Typography>
+            <Grid container className={classes.root} spacing={2}>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={0}>
+                        {[0, 1, 2].map((value) => (
+                            <Grid key={value} item>
+                                <OutlinedCard filename={"dw"} date={"12/06/2020"} records={50} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
+        </div>
+    )
+}
